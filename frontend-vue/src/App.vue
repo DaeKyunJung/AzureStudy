48f6438f3813c0b88315bf62a5d2df04969a8734
DK <diekdaldzl@hanmail.net>
2024-08-02 16:42:28 +0900

    2024_08_02

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <div>수정완료</div>
+    <div>Azure 교육_20240802</div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
